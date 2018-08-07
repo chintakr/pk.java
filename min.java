4803bc36f995cbf9ad7d6756aeb6681a20765f2c
chintakr <41940030+chintakr@users.noreply.github.com>
2018-08-07 13:02:06 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i,k;
+		Scanner s=new Scanner(System.in);
+		n = s.nextInt();
+		int arr[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			arr[i]=s.nextInt();
+			
+		}
+		k=arr[0];
+		for(i=0;i<n;i++){
+			if(arr[i]<=k)
+			{
+				k=arr[i];
+	
+			}
+			
+		}
+		System.out.println(k);
+		
+	}
+}
